e3167a4f06b1aece17cc103641d8848480b3e82d
syedtahmidhasin <hasintahmid0@gmail.com>
2025-12-24 17:03:29 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+my_name = "Tahmid"
+birth_day = 1
+a = 2025
+b = 2008
+diff = a-b
+print( "My name is" ,my_name)
+print( f"My date of  is May {birth_day}  uhdjh vish {diff}")
+print( f"i am {diff} years old" ) 
