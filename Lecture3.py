5945a1678cc964f19cf0c8a0a38aef8043baec45
syedtahmidhasin <hasintahmid0@gmail.com>
2025-12-31 19:00:00 +0600

    Lecture3.py

--- Lecture3.py
+++ Lecture3.py
@@ -1,1 +1,9 @@
 #Lists
+marks = [99.3, 96.2, 92.3]
+print(marks[0])
+print(len(marks))
+
+student = ["Tashrik", 25, 5.9]
+print(student[0])
+student[0] = "Tahmid" #list er kono value change kora
+print(student)
