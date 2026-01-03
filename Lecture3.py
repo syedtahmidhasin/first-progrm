52f286b22a8a48a077f566b93b4a5a07ddd52d85
syedtahmidhasin <hasintahmid0@gmail.com>
2026-01-03 15:59:22 +0600

    Update Lecture3.py

--- Lecture3.py
+++ Lecture3.py
@@ -34,3 +34,58 @@ list.insert(1, 3) #kono 1ta particular indx e 1ta eliment add kora
 print(list1)
 list.pop(0) #kono 1ta particular indx ke remove kora
 list.remove(4) #kono 1ta eliment ke remove kora(jodi oi eliment onkbar thake tahole age jeita thakbe oita remove hbe)
+
+#Tuple
+tup = (9, 5, 3, 6, 2)
+print(type(tup))
+print(tup[0]) #indexing
+print(tup[0:4]) #slicing
+print(tup[0:]) #0:len(tup) er moto
+print(tup[:4]) #0:4 er moto
+
+tup1 = ("hello",) #1ta eliment thekle comma dite hbe nahole python oitake str/float/int hishebe nibe
+print(type(tup1))
+
+#Tuple method
+tup = (2, 1, 3, 1)
+tup.index(1) #kono eliment jei indx e age thakbe oi indx dekhabe
+tup.count(1) #ono eliment oi tuple e koybar ase 
+print(tup.index(1))
+print(tup.count(1))
+
+#Practice
+movie1 = input("First movie name :")
+movie2 = input("Second movie name :")
+movie3 = input("Third movie name :")
+
+Favourite_movie_list = []
+Favourite_movie_list.append(movie1)
+Favourite_movie_list.append(movie2)
+Favourite_movie_list.append(movie3)
+
+print(Favourite_movie_list)
+
+List1 = [1, 2, 1]
+List2 = [1, 2, 3]
+
+copy_List1 = List1.copy()
+copy_List1.reverse()
+if(List1 == copy_List1):
+    print("It's a palindrome")
+else:
+    print("It's not a palindrome")
+
+copy_List2 = List2.copy()
+copy_List2.reverse()
+if(List2 == copy_List2):
+    print("It's a palindrome")
+else:
+    print("It's not a palindrome")
+
+Grade = ("C", "D", "A", "A", "B", "B", "A")
+A = Grade.count("A")
+print("Num of A :", A)
+
+Grade = ["C", "D", "A", "A", "B", "B", "A"]
+Grade.sort()
+print(Grade)
