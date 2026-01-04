14f488c8b80ca8b1b4236693f35ac08094b25b38
syedtahmidhasin <hasintahmid0@gmail.com>
2026-01-04 18:04:53 +0600

    Create Lecture4.py

---
+++ Lecture4.py
@@ -0,0 +1,15 @@
+#Dictionary
+info = {
+    "name" : "Tahmid",
+    "age" : 12,
+    True : 99.9,
+    57.3 : ["Math", "Physics"],
+    69 : ("algebra", "force")
+}
+print(info)
+print(type(info))
+print(info["age"]) #dict er kono key er value print kora
+print(info["name"])
+info["marks"] = 100 #dict er kono value change kora
+info["grade"] = "A" #dict e kono new key & value add kora
+print(info)
