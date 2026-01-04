299f9e08f5e28e6d6e72adeac4e37063a2df2344
syedtahmidhasin <hasintahmid0@gmail.com>
2026-01-04 18:13:15 +0600

    Update Lecture4.py

--- Lecture4.py
+++ Lecture4.py
@@ -13,3 +13,14 @@ print(info["name"])
 info["marks"] = 100 #dict er kono value change kora
 info["grade"] = "A" #dict e kono new key & value add kora
 print(info)
+
+#Nested dictionary
+student = {
+    "name" : "Tahmid",
+    "marks in sub" : {
+        "phy" : 98,
+        "chem" : 96,
+        'bio' : 97
+    }
+}
+print(student["marks in sub"]["phy"])
