1f677d73828fd9cd03d6d69f5d8f316a0712dbd8
syedtahmidhasin <hasintahmid0@gmail.com>
2026-01-10 20:54:28 +0600

    Create Lecture5.py

---
+++ Lecture5.py
@@ -0,0 +1,24 @@
+#Loops
+i = 1
+while i <= 5: #jotokkhon count er value <= 5 thakbe totokkhon print korbe
+    print(i)
+    i += 1 #totokkhon porjonto count er shathe 1 add korbe jotokkhon na count er value <= 5 hoy
+    
+print("The loop is end")
+
+#Practice
+i = 1
+while i <= 100:
+    print(i)
+    i += 1
+
+i = 100
+while i >= 1:
+    print(i)
+    i -= 1
+
+n = int(input("enter number :"))
+i = 1
+while i <= 10:
+    print(n * i)
+    i += 1
