58382838ab46c72e10b4d821315066169d2bd791
syedtahmidhasin <hasintahmid0@gmail.com>
2026-01-19 19:03:15 +0600

    Update Lecture6.py

--- Lecture6.py
+++ Lecture6.py
@@ -61,3 +61,13 @@ def fact(n):
     else:
         return n * fact(n-1)
 print(fact(3))
+
+#Practice
+def calc_sum(n):
+    if(n == 0):
+        return 0
+    return calc_sum(n-1) + n
+
+sum = calc_sum(5)
+print(sum)
+
